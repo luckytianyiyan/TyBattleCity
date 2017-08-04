62414d48b60d1af5429b4ce1a96cf288cc895519
luckytianyiyan <luckytianyiyan@gmail.com>
2017-08-04 14:22:46 +0800

    Add Player Control View

---
+++ TyBattleCity/ControlViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ControlViewController.swift
+//  TyBattleCity
+//
+//  Created by luckytianyiyan on 2017/8/4.
+//  Copyright © 2017年 luckytianyiyan. All rights reserved.
+//
+
+import UIKit
+
+class ControlViewController: UIViewController {
+    
+    @IBAction func onUpButtonClick(_ sender: Any) {
+        GameController.shared.trun(to: .up)
+    }
+    
+    @IBAction func onRightButtonClick(_ sender: Any) {
+        GameController.shared.trun(to: .right)
+    }
+    
+    @IBAction func onDownButtonClick(_ sender: Any) {
+        GameController.shared.trun(to: .down)
+    }
+    
+    @IBAction func onLeftButtonClick(_ sender: Any) {
+        GameController.shared.trun(to: .left)
+    }
+}
